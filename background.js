c682095cfce8d39f2623c4eea607f240863f98ec
parichkova <tyhomiraparichkova@gmail.com>
2018-06-24 16:41:12 +0300

    added functionality for destroying modal and injecting its scripts only once.

--- background.js
+++ background.js
@@ -1,14 +1,19 @@
-//you will see this message on plugin installation
+let loaded = false;
 
+//you will see this message on plugin installation
 chrome.runtime.onInstalled.addListener(function() {
   alert('Installed');
 });
 
 //when clickedx on the extension icon, logic.js code will be executed
 chrome.browserAction.onClicked.addListener(function(tab) { 
-  //alert('icon clicked');
-  chrome.tabs.executeScript(tab.id, {file: 'dragElementLogic.js'});
-  chrome.tabs.executeScript(tab.id, {file: 'logic.js'});
 
+  alert('Click on the text you would like to translate.');
+
+  if (!loaded) {
+    chrome.tabs.executeScript(tab.id, {file: 'dragElementLogic.js'});
+    chrome.tabs.executeScript(tab.id, {file: 'logic.js'});  
+    loaded = true;
+  }
 });
 
